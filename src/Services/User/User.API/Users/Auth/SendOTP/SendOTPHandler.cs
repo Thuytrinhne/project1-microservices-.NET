a7ba6a51a8fcd131f08b674e2dc9080ffd6ed719
NATNgoc <96611889+NATNgoc@users.noreply.github.com>
2024-07-06 22:23:10 +0700

    Fix send email
    fix(User): Fix not sending email when running on docker

--- src/Services/User/User.API/Users/Auth/SendOTP/SendOTPHandler.cs
+++ src/Services/User/User.API/Users/Auth/SendOTP/SendOTPHandler.cs
@@ -1,28 +1,23 @@
-
-using Microsoft.Extensions.Options;
-using MongoDB.Driver;
-
-
 namespace User.API.Users.Auth.SendOTP
 {
-    public record SendOTPCommand (string Email) :ICommand<SendOTPResult>;
+    public record SendOTPCommand(string Email) : ICommand<SendOTPResult>;
     public record SendOTPResult(bool IsSuccess);
 
     public class SendOTPCommandHandler
         (EmailVerificationService _service, UserManager<ApplicationUser> _userManager, IEmailSender _emailSender)
         : ICommandHandler<SendOTPCommand, SendOTPResult>
     {
-        
+
         public async Task<SendOTPResult> Handle(SendOTPCommand command, CancellationToken cancellationToken)
         {
             var user = await _userManager.FindByEmailAsync(command.Email);
             if (user is not null)
             {
-                return new  SendOTPResult(false);
+                return new SendOTPResult(false);
             }
             string otp = GenerateOTP();
             await SendOTPEmail(command.Email, otp);
-            await  _service.CreateAsync(new EmailVerification { OTP = otp, Email = command.Email, ExpiryTime = DateTime.UtcNow.AddMinutes(Authorization.OTPExpiredTimeInMinutes)});
+            await _service.CreateAsync(new EmailVerification { OTP = otp, Email = command.Email, ExpiryTime = DateTime.UtcNow.AddMinutes(Authorization.OTPExpiredTimeInMinutes) });
             return new SendOTPResult(true);
 
 
@@ -36,7 +31,7 @@ namespace User.API.Users.Auth.SendOTP
         {
             string subject = "Comestic Store- Sending OTP";
             string message = $"Your OTP is {otp}.\n Note that this OTP is valid within {Authorization.OTPExpiredTimeInMinutes} mins.";
-            await _emailSender.SendEmailAsync(email, subject, message);
+            _emailSender.SendEmailAsync(email, subject, message);
         }
     }
 }
