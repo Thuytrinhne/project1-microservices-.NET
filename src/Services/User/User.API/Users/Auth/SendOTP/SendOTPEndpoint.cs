a2d3f96d10162111282804318c63dcd2167f6071
Nguyễn Thùy Trinh <113348581+Thuytrinhne@users.noreply.github.com>
2024-12-07 13:38:30 +0700

    Update SendOTPEndpoint.cs

--- src/Services/User/User.API/Users/Auth/SendOTP/SendOTPEndpoint.cs
+++ src/Services/User/User.API/Users/Auth/SendOTP/SendOTPEndpoint.cs
@@ -18,7 +18,7 @@ namespace User.API.Users.Auth.SendOTP
                 var response = result.Adapt<SendOTPResponse>();
                 if (response.IsSuccess)
                 {
-                    return Results.Ok(new SendOTPResponse(true, "Test cd"));
+                    return Results.Ok(new SendOTPResponse(true, "Test green"));
                 }
                 return Results.BadRequest(response);
             })
